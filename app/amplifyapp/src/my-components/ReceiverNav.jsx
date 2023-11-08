91f3a8fb09fb08904545c418470a7d7a75c23b6b
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-11-07 16:20:26 -0800

    Add greeting in header

--- app/amplifyapp/src/my-components/ReceiverNav.jsx
+++ app/amplifyapp/src/my-components/ReceiverNav.jsx
@@ -1,18 +1,24 @@
 import * as React from "react";
-import {Tabs, Flex, Heading, Link, Button, useTheme, View, Text, Icon, TabItem} from "@aws-amplify/ui-react";
-import { HiHome  } from 'react-icons/hi';
+import { Tabs, Flex, Heading, Link, Button, useTheme, View, Text, Icon, TabItem } from "@aws-amplify/ui-react";
+import { HiHome } from 'react-icons/hi';
 
 export const ReceiverNav = (props) => {
-    const {tokens} = useTheme();
-    return(
-       <View backgroundColor={tokens.colors.primary} padding={'.2rem 2rem'} boxShadow={'small'}> 
-            <Flex direction = 'row' justifyContent = 'space-between'>
-                <Link alignSelf = 'center'><Icon ariaLabel="Javascript" as={HiHome} /> Home</Link>
+    const { tokens } = useTheme();
+    const [isLoggedIn, setIsLoggedIn] = React.useState(false)
+    return (
+        <View backgroundColor={tokens.colors.primary} padding={'.2rem 2rem'} boxShadow={'small'}>
+            <Flex direction='row' justifyContent='space-between'>
+                <Link alignSelf='center'><Icon ariaLabel="Javascript" as={HiHome} /> Home</Link>
+                <Text alignSelf='center'>{props.name !== null ?  "Hello " + props.name : null}</Text>
                 <View>
-                    <Button variation="link" label = 'Log in' marginRight={'1rem'}> Log in </Button>
-                    <Button variation="primary" label = 'Sign up'> Sign Up </Button>
+                    {!isLoggedIn ?
+                        <Button variation="link" label='Log in' marginRight='1rem'> Log in </Button>
+                        : null}
+                    {!isLoggedIn ?
+                        <Button variation="primary" label='Sign up'> Sign Up </Button>
+                        : null}
                 </View>
-            </Flex>   
-       </View>
+            </Flex>
+        </View>
     )
 }
