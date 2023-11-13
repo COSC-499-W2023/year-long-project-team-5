f55f7fe577981a5d5e1e27fbb307277140a44fba
Abhinav Thota <abhinavthota01@gmail.com>
2023-11-12 20:07:31 -0800

    tweaked some looks of the ui components, navbar (in Layout.js) is now displaying the username. In the future this may need to be seperate components for responsive design.

--- app/amplifyapp/src/pages/Layout.js
+++ app/amplifyapp/src/pages/Layout.js
@@ -4,12 +4,12 @@ import * as React from "react";
 import {Button, useTheme, useAuthenticator,View, Flex, Tabs, TabItem, Text, Link as AmplifyLink} from '@aws-amplify/ui-react';
 
 export function Layout(){
-    const {route, signOut} = useAuthenticator((context) => [
+    const {user, route, signOut} = useAuthenticator((context) => [
+        context.user,
         context.route,
-        context.signOut
+        context.signOut,
     ]);
     const navigate = useNavigate();
-
     function logOut() {
         signOut();
         navigate('/login');
@@ -17,7 +17,7 @@ export function Layout(){
     const {tokens} = useTheme();
     return(
         <View className="App">
-            <Flex boxShadow={tokens.shadows.medium} padding={tokens.space.small} justifyContent='space-between' alignItems='center'>
+            <Flex boxShadow={tokens.shadows.medium} padding={tokens.space.small} justifyContent='space-between' alignItems='center' marginBottom={tokens.space.large}>
                 <Flex direction="row">
                     <AmplifyLink onClick={()=> navigate('/')}>Home</AmplifyLink>
                     <AmplifyLink onClick={()=> navigate('/Dashboard')}> Dashboard</AmplifyLink>
@@ -27,7 +27,7 @@ export function Layout(){
                     <Button onClick={() => navigate('/Login')}> Login</Button>
                 ):
                     <Flex direction='row' alignItems='center'>
-                        <Text>Hello, user!</Text>
+                        <Text>Hello, {user.attributes.name}!</Text>
                         <Button onClick={() => logOut()}> Sign Out</Button>
                     </Flex>
                 }
