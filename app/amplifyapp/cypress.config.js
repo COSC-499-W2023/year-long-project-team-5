1f2b4536d944f4186ab76139c82b6ee855098cdb
Josh Medina <joshuamed27@gmail.com>
2023-11-20 16:39:42 -0800

    Add e2e testing and change proj config

--- app/amplifyapp/cypress.config.js
+++ app/amplifyapp/cypress.config.js
@@ -1,11 +1,18 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "dvzyoy",
+  projectId: "ec7qmf",
+
   component: {
     devServer: {
       framework: "create-react-app",
       bundler: "webpack",
+    },
+  },
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
     },
   },
 });
