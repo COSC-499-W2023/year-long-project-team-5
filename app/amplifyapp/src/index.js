b488f32ef07a08d545e0e7d567ce22c647bfa1f7
abbykiehlbauch <abbykiehlbauch@gmail.com>
2023-11-22 12:32:30 -0800

    cleaning up white space

--- app/amplifyapp/src/index.js
+++ app/amplifyapp/src/index.js
@@ -7,8 +7,6 @@ import { Amplify } from 'aws-amplify';
 import config from './aws-exports';
 Amplify.configure(config);
 
-
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
