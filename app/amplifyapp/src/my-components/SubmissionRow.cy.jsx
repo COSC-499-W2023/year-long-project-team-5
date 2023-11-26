924079e88acf181c1b503d22acf2953fe35376a1
Abhinav Thota <abhinavthota01@gmail.com>
2023-11-25 23:59:03 -0800

    wrote test to check open submission link button; need to code it next

--- app/amplifyapp/src/my-components/SubmissionRow.cy.jsx
+++ app/amplifyapp/src/my-components/SubmissionRow.cy.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { SubmissionRow } from './SubmissionRow'
-
-const fakeSubmission = {id: 1, email: 'kenaa@example.com', dateSent: '2020-01-01T00:00:00', dateReceived: '2020-01-03T00:00:00',videoLink: 'https://www.youtube.com/watch?v=dQw4w9'}
-
+const expVidURL = 'https://www.w3schools.com/howto/howto_website_static.asp'
+const fakeSubmission = {id: 1, email: 'kenaa@example.com', dateSent: '2020-01-01T00:00:00', dateReceived: '2020-01-03T00:00:00',videoLink: expVidURL}
+const expSubURL = ''
 describe('<SubmissionRow/>', () => {
     it('renders SubmissionRow', () => {
       // see: https://on.cypress.io/mounting-react
@@ -19,7 +19,28 @@ describe('<SubmissionRow/>', () => {
       cy.get('.subDS').eq(0).should('contains.text', '2020-01-01T00:00:00')
       cy.get('.subDR').eq(0).should('contains.text', '2020-01-03T00:00:00')
       cy.get('.vidLink').eq(0).should('contains.text', 'Video Link')
-      cy.get('.vidLink :first-child').eq(0).should('have.attr', 'href').and('include', 'https://www.youtube.com/watch?v=dQw4w9')
+      cy.get('.vidLink :first-child').eq(0).should('have.attr', 'href').and('include', expVidURL)
+      //cy.get('.fullLink').eq(0).should('exist').and('have.attr', 'href')
+    })
+    //doesn't work as a test
+    // it('check if video link when clicked, opens the correct link', () => {
+    //   cy.mount(<SubmissionRow id = {fakeSubmission.id} email = {fakeSubmission.email} 
+    //     dateSent = {fakeSubmission.dateSent} dateReceived = {fakeSubmission.dateReceived} videoLink = {fakeSubmission.videoLink} />)
+    //   cy.get('.vidLink :first-child').eq(0).click()
+    //   .then((openedUrl) => {
+    //     expect(openedUrl).to.eq(expVidURL);
+    //     cy.go('back')
+    //   })
+    // })
+    it('check if full submission link works and opens a new page', () =>{
+      cy.mount(<SubmissionRow id = {fakeSubmission.id} email = {fakeSubmission.email} 
+               dateSent = {fakeSubmission.dateSent} dateReceived = {fakeSubmission.dateReceived} videoLink = {fakeSubmission.videoLink} />)
+      cy.get('.fullLink :first-child').eq(0).click().then(
+        () => {
+          cy.url().should('equal', expSubURL)
+        }
+      )
+      cy.go('back')
     })
   })
 
