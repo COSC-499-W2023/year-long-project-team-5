e75b0df4e88f4c64722e0ab1062ca69582bb7c7c
Abhinav Thota <abhinavthota01@gmail.com>
2023-11-25 18:54:04 -0800

    modified component to take props

--- app/amplifyapp/src/my-components/SubmissionRow.jsx
+++ app/amplifyapp/src/my-components/SubmissionRow.jsx
@@ -4,6 +4,12 @@ import {Card, Flex, Heading, Link, Button, useTheme, View, Text} from "@aws-ampl
 export const SubmissionRow = (props) => {
     const {tokens} = useTheme();
     return(
-        <View>Hi</View>
+        <View>
+            <Text> props.id</Text>
+            <Text> props.email</Text>
+            <Text> props.dateSent</Text>
+            <Text> props.dateReceived</Text>
+            <Link href={props.videoLink}>Video Link</Link>
+        </View>
     );
 }
