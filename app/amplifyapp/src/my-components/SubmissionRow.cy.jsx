8972eb86328128da3ba5f1ec92d2e1e99db47620
Abhinav Thota <abhinavthota01@gmail.com>
2023-11-25 19:21:49 -0800

    cypress test able to find and check if values appear, need to figure out how to test props and event handlers next...

--- app/amplifyapp/src/my-components/SubmissionRow.cy.jsx
+++ app/amplifyapp/src/my-components/SubmissionRow.cy.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { SubmissionRow } from './SubmissionRow'
 
-const fakeSubmission = {id: 1, email: 'kenaa@example.com', dateSent: '2020-01-01T00:00:0', dateReceived: '2020-01-03T00:00',videoLink: 'https://www.youtube.com/watch?v=dQw4w9'}
+const fakeSubmission = {id: 1, email: 'kenaa@example.com', dateSent: '2020-01-01T00:00:00', dateReceived: '2020-01-03T00:00:00',videoLink: 'https://www.youtube.com/watch?v=dQw4w9'}
 
 describe('<SubmissionRow/>', () => {
     it('renders SubmissionRow', () => {
@@ -12,15 +12,17 @@ describe('<SubmissionRow/>', () => {
     })
     // if each row has: id, email, date sent, date received, and video submission link
     it('each row has values in props: id, email, date sent, date received, and video submission link', () => {
-      cy.get('[data-cy=id').should('have.text', fakeSubmission.id);
-      cy.get('[data-cy=email]').should('have.text', fakeSubmission.email);
-      cy.get('[data-cy=dateSent]').should('have.text', fakeSubmission.dateSent);
-      cy.get('[data-cy=dateReceived]').should('have.text', fakeSubmission.dateReceived);
-      cy.get('[data-cy=videoLink]').should('have.text', 'Video Link');
-      cy.get('[data-cy=videoLink]').should('have.attr', 'href', fakeSubmission.videoLink);
+      cy.mount(<SubmissionRow id = {fakeSubmission.id} email = {fakeSubmission.email} 
+      dateSent = {fakeSubmission.dateSent} dateReceived = {fakeSubmission.dateReceived} videoLink = {fakeSubmission.videoLink} />)
+      cy.contains('1').should('be.visible');
+      cy.contains('kenaa@example.com').should('be.visible');
+      cy.contains('2020-01-01T00:00:00').should('be.visible');
+      cy.contains('2020-01-03T00:00:00').should('be.visible');
+      cy.contains('Video Link').should('be.visible');
+      // cy.contains('https://www.youtube.com/watch?v=dQw4w9').should('be.visible');
     })
-    // if video link opens the correct webpage containing the video
-    it('renders SubmissionRow', () => {
-      cy.get('[data-cy=')
-    })
+    // // if video link opens the correct webpage containing the video
+    // it('renders SubmissionRow', () => {
+    //   cy.get('[data-cy=')
+    // })
   })
--- app/amplifyapp/src/my-components/SubmissionRow.jsx
+++ app/amplifyapp/src/my-components/SubmissionRow.jsx
@@ -5,10 +5,10 @@ export const SubmissionRow = (props) => {
     const {tokens} = useTheme();
     return(
         <View>
-            <Text> props.id</Text>
-            <Text> props.email</Text>
-            <Text> props.dateSent</Text>
-            <Text> props.dateReceived</Text>
+            <Text> {props.id}</Text>
+            <Text> {props.email}</Text>
+            <Text> {props.dateSent}</Text>
+            <Text> {props.dateReceived}</Text>
             <Link href={props.videoLink}>Video Link</Link>
         </View>
     );
