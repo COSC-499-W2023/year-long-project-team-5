a22621ddfa77ff0bdb138c9701f4fbe9ad52903e
abbykiehlbauch <abbykiehlbauch@gmail.com>
2023-11-27 15:41:23 -0800

    small changes

--- app/amplifyapp/src/pages/Home.js
+++ app/amplifyapp/src/pages/Home.js
@@ -9,16 +9,16 @@ export function Home(){
 				<div>
 				<img src = {process.env.PUBLIC_URL + "/full-logo.png"}/>
 				</div>
-			<Flex alignItems = 'center' direction = 'row' justifyContent = 'center' wrap = "wrap">
-				<View height="12em" width="33rem" backgroundColor={tokens.colors.blue[20]} justifyContent = 'center' margin = 'auto'>
+			<Flex alignItems = 'center' direction = 'row' justifyContent = 'center' wrap = "wrap" margin = '1rem 0rem 1rem 0rem'>
+				<View height="12em" width="25rem" backgroundColor={tokens.colors.blue[20]} justifyContent = 'center' margin = 'auto'>
 					<h1 font-family>
 						User-friendly
 					</h1>
 				</View>
-				<View height="12rem" width="33rem" backgroundColor={tokens.colors.blue[40]} margin = 'auto'>
+				<View height="12rem" width="25rem" backgroundColor={tokens.colors.blue[40]} margin = 'auto'>
 					<h1>Open Source</h1>
 				</View>
-				<View height="12rem" width="33rem" backgroundColor={tokens.colors.blue[60]} margin = 'auto'>
+				<View height="12rem" width="25rem" backgroundColor={tokens.colors.blue[60]} margin = 'auto'>
 					<h1>Asynchronous communication</h1>
 				</View>
 			</Flex>
