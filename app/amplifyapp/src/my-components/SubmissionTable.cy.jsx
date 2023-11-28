46651babd74f47a4a41488ad580297f5d21ae1f8
Abhinav Thota <abhinavthota01@gmail.com>
2023-11-27 21:41:22 -0800

    fixed up test

--- app/amplifyapp/src/my-components/SubmissionTable.cy.jsx
+++ app/amplifyapp/src/my-components/SubmissionTable.cy.jsx
@@ -10,7 +10,7 @@ context('Laptop Test', () => {
     })
     it('renders SubmsisionTable and see that each table header contains expected value', () => {
         cy.mount(<SubmissionTable></SubmissionTable>)
-        cy.get('.tableHeader').eq(0).should('contains.text', 'Customer ID')
+        cy.get('.tableHeader').eq(0).should('contains.text', 'Submission #')
         cy.get('.tableHeader').eq(1).should('contains.text', 'Customer Email')
         cy.get('.tableHeader').eq(2).should('contains.text', 'Date Sent')
         cy.get('.tableHeader').eq(3).should('contains.text', 'Date Received')
