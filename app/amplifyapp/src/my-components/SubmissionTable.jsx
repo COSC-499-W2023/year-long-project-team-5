23cb194c07688983b7c5eb7c3bab01c410702936
Abhinav Thota <abhinavthota01@gmail.com>
2023-11-28 17:02:35 -0800

    added docs

--- app/amplifyapp/src/my-components/SubmissionTable.jsx
+++ app/amplifyapp/src/my-components/SubmissionTable.jsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import {Table, TableRow, TableHead, TableCell, TableBody} from '@aws-amplify/ui-react'
 
-export const SubmissionTable = (props) => {
 /**
- * @params {Object} SubmissionTable
- * @params {TableHead} 
- * 
+ * @component {Object} SubmissionTable
+ * @example 
+ * //Example usage of SubmissionTable being used with one SubmissionRow being passed as a prop 
+ * <SubmissionTable rowsToDisplay = {<SubmissionRow id = '1' email = 'abq1a@gmail.com'  dateSent='2020-01-01T00:00:00' dateReceived = '2020-01-01T00:00:00' videoLink = 'videotoshow'/>}/>
+ *
+ * @param {Object} rowsToDisplay - takes in rows to display (can take multiple through array or map function)
+ * @returns JSX.Element
  */
+
+export const SubmissionTable = (props) => {
     return(
         <Table>
             <TableHead>
