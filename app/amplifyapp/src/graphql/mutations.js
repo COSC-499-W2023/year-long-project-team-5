f2f256aeac981dd96543cb9fdcc88ffa637e2cdf
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-11-30 22:40:41 -0800

    Update graphql queries with codegen

--- app/amplifyapp/src/graphql/mutations.js
+++ app/amplifyapp/src/graphql/mutations.js
@@ -1,21 +1,105 @@
 /* eslint-disable */
 // this is an auto generated file. This will be overwritten
 
-export const createNote = /* GraphQL */ `
-  mutation CreateNote(
-    $input: CreateNoteInput!
-    $condition: ModelNoteConditionInput
+export const createSubmission = /* GraphQL */ `
+  mutation CreateSubmission(
+    $input: CreateSubmissionInput!
+    $condition: ModelSubmissionConditionInput
   ) {
-    createNote(input: $input, condition: $condition) {
+    createSubmission(input: $input, condition: $condition) {
       id
-      image
+      adminId
+      Video {
+        id
+        videoURL
+        createdAt
+        updatedAt
+        __typename
+      }
+      User {
+        id
+        email
+        name
+        createdAt
+        updatedAt
+        __typename
+      }
+      note
+      submittedAt
       createdAt
       updatedAt
+      submissionVideoId
+      submissionUserId
       __typename
     }
   }
 `;
-
+export const updateSubmission = /* GraphQL */ `
+  mutation UpdateSubmission(
+    $input: UpdateSubmissionInput!
+    $condition: ModelSubmissionConditionInput
+  ) {
+    updateSubmission(input: $input, condition: $condition) {
+      id
+      adminId
+      Video {
+        id
+        videoURL
+        createdAt
+        updatedAt
+        __typename
+      }
+      User {
+        id
+        email
+        name
+        createdAt
+        updatedAt
+        __typename
+      }
+      note
+      submittedAt
+      createdAt
+      updatedAt
+      submissionVideoId
+      submissionUserId
+      __typename
+    }
+  }
+`;
+export const deleteSubmission = /* GraphQL */ `
+  mutation DeleteSubmission(
+    $input: DeleteSubmissionInput!
+    $condition: ModelSubmissionConditionInput
+  ) {
+    deleteSubmission(input: $input, condition: $condition) {
+      id
+      adminId
+      Video {
+        id
+        videoURL
+        createdAt
+        updatedAt
+        __typename
+      }
+      User {
+        id
+        email
+        name
+        createdAt
+        updatedAt
+        __typename
+      }
+      note
+      submittedAt
+      createdAt
+      updatedAt
+      submissionVideoId
+      submissionUserId
+      __typename
+    }
+  }
+`;
 export const createVideo = /* GraphQL */ `
   mutation CreateVideo(
     $input: CreateVideoInput!
@@ -30,7 +114,96 @@ export const createVideo = /* GraphQL */ `
     }
   }
 `;
-
+export const updateVideo = /* GraphQL */ `
+  mutation UpdateVideo(
+    $input: UpdateVideoInput!
+    $condition: ModelVideoConditionInput
+  ) {
+    updateVideo(input: $input, condition: $condition) {
+      id
+      videoURL
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteVideo = /* GraphQL */ `
+  mutation DeleteVideo(
+    $input: DeleteVideoInput!
+    $condition: ModelVideoConditionInput
+  ) {
+    deleteVideo(input: $input, condition: $condition) {
+      id
+      videoURL
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const createUser = /* GraphQL */ `
+  mutation CreateUser(
+    $input: CreateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    createUser(input: $input, condition: $condition) {
+      id
+      email
+      name
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const updateUser = /* GraphQL */ `
+  mutation UpdateUser(
+    $input: UpdateUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    updateUser(input: $input, condition: $condition) {
+      id
+      email
+      name
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const deleteUser = /* GraphQL */ `
+  mutation DeleteUser(
+    $input: DeleteUserInput!
+    $condition: ModelUserConditionInput
+  ) {
+    deleteUser(input: $input, condition: $condition) {
+      id
+      email
+      name
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
+export const createNote = /* GraphQL */ `
+  mutation CreateNote(
+    $input: CreateNoteInput!
+    $condition: ModelNoteConditionInput
+  ) {
+    createNote(input: $input, condition: $condition) {
+      id
+      name
+      description
+      image
+      viewedStatus
+      createdAt
+      updatedAt
+      __typename
+    }
+  }
+`;
 export const updateNote = /* GraphQL */ `
   mutation UpdateNote(
     $input: UpdateNoteInput!
@@ -41,6 +214,7 @@ export const updateNote = /* GraphQL */ `
       name
       description
       image
+      viewedStatus
       createdAt
       updatedAt
       __typename
@@ -57,6 +231,7 @@ export const deleteNote = /* GraphQL */ `
       name
       description
       image
+      viewedStatus
       createdAt
       updatedAt
       __typename
