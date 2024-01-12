dca7bf8de94ce4b7937d112b36b88e1ee07c7800
abbykiehlbauch <abbykiehlbauch@gmail.com>
2024-01-12 14:12:55 -0800

    added video background and animations

--- app/amplifyapp/src/pages/Home.js
+++ app/amplifyapp/src/pages/Home.js
@@ -9,21 +9,34 @@ import {
 	Button,
 	useTheme,
   } from '@aws-amplify/ui-react';
-  
+  import { Divider } from '@aws-amplify/ui-react';
+  import videoBG2 from '../assets/videoBG2.mp4';
+  import {motion} from "framer-motion";
+
+
   export const Home = () => {
 	const { tokens } = useTheme();
 	return (
 		<div>
-			<div>
-				<img src = {process.env.PUBLIC_URL + "/full-logo.png"} width = '25%'/>
+			<div className = "wrapper">
+			<div className = "overlay"></div>
+			<video src = {videoBG2} autoPlay loop muted width = "100%"/>
+			<div className = "content">
+				<h1>blur</h1>
+				<h2>Asynchronous, secure, and efficient communication</h2>
 			</div>
-		<Flex alignItems = 'center' direction = 'row' justifyContent = 'center' wrap = "wrap" margin = '1rem 0rem 1rem 0rem'>
-			<View
-				backgroundColor= {tokens.colors.blue[20]}
-				padding={tokens.space.medium}
+			</div>
+			<motion.div
+				initial = {{opacity: 0}}
+				whileInView = {{opacity:1}}
+				exit = {{opacity: 0}}
+				viewport={{amount: 0.8, once: false}}
 			>
-				<Card width = '20rem' height = '12rem'>
-<Flex direction="column" alignItems="flex-start"  textAlign="left">
+		<Flex alignItems = 'center' direction = 'row' justifyContent = 'center' wrap = "wrap" margin = '1rem 0rem 1rem 0rem'>
+			<View padding={tokens.space.medium}>
+				<Card width = '20rem' height = '12rem' border-top = '1px'>
+				<Flex direction="column" alignItems="flex-start"  textAlign="left">
+				<Divider/>
 						<Heading level={5}>
 							User-friendly
 						</Heading>
@@ -35,37 +48,39 @@ import {
 				</Card>
 			</View>
 			<View
-				backgroundColor= {tokens.colors.blue[20]}
 				padding={tokens.space.medium}
 			>
 				<Card width = '20rem' height = '12rem'>
-<Flex direction="column" alignItems="flex-start"  textAlign="left">
-						<Heading level={5}>
-							Asynchronous communication
-						</Heading>
-						<Text as="span">
-							This software allows you to have asynchronous communication with your contacts, allowing
-							for efficient and convenient communication.
-						</Text>
-					</Flex>
-				</Card>
+				<Flex direction="column" alignItems="flex-start"  textAlign="left">
+					<Divider/>
+					<Heading level={5}>
+						Asynchronous communication
+					</Heading>
+					<Text as="span">
+						This software allows you to have asynchronous communication with your contacts, allowing
+						for efficient and convenient communication.
+					</Text>
+				</Flex>
+			</Card>
 			</View>
 			<View
-				backgroundColor= {tokens.colors.blue[20]}
 				padding={tokens.space.medium}
+				border-top = "1px solid #669999"
 			>
-				<Card width = '20rem' height = '12rem'>
-				<Flex direction="column" alignItems="flex-start">
+				<Card width = '20rem' height = '12rem' border-top = "1px solid black">
+				<Flex direction="column" alignItems="flex-start" textAlign="left">
+					<Divider/>
 					<Heading level={5}>
 						Open Source
 					</Heading>
 						<Text as="span">
-						This software is open source and developed using AWS services such as Amplify, Cognito, and Rekognition.
+							This software is open source and developed using AWS services such as Amplify, Cognito, and Rekognition.
 						</Text>
 					</Flex>
 				</Card>
 			</View>
 	  	</Flex>
+		</motion.div>
 	</div>
 	);
   };
