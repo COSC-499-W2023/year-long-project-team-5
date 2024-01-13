70e28fc7be79e8f94b7176c11ff5b5979b1b9850
MooMan <36575231+Namesnipes@users.noreply.github.com>
2024-01-13 15:36:20 -0800

    formatting and imports

--- app/amplifyapp/src/pages/Layout.js
+++ app/amplifyapp/src/pages/Layout.js
@@ -1,7 +1,6 @@
 import { Outlet, useNavigate } from "react-router-dom";
-import {NavBar} from '../my-components/NavBar';
 import * as React from "react";
-import {Button, useTheme, useAuthenticator,View, Flex, Tabs, TabItem, Text, Link as AmplifyLink} from '@aws-amplify/ui-react';
+import { Button, useTheme, useAuthenticator, View, Flex, Link as AmplifyLink } from '@aws-amplify/ui-react';
 
 /**
  * Layout TODO: finish docs
@@ -10,8 +9,8 @@ import {Button, useTheme, useAuthenticator,View, Flex, Tabs, TabItem, Text, Link
  * @example
  * <Layout></Layout>
  */
-export function Layout(){
-    const {user, route, signOut} = useAuthenticator((context) => [
+export function Layout() {
+    const { user, route, signOut } = useAuthenticator((context) => [
         context.user,
         context.route,
         context.signOut,
@@ -21,25 +20,25 @@ export function Layout(){
         signOut();
         navigate('/login');
     }
-    const {tokens} = useTheme();
-    return(
+    const { tokens } = useTheme();
+    return (
         <View className="App">
             <Flex boxShadow={tokens.shadows.medium} padding={tokens.space.small} justifyContent='space-between' alignItems='center' marginBottom={tokens.space.large}>
                 <Flex direction="row">
-                    <AmplifyLink onClick={()=> navigate('/')}>Home</AmplifyLink>
-                    <AmplifyLink onClick={()=> navigate('/Dashboard')}> Dashboard</AmplifyLink>
-                    <AmplifyLink onClick={()=> navigate('/Submission')}>Request a video</AmplifyLink>
+                    <AmplifyLink onClick={() => navigate('/')}>Home</AmplifyLink>
+                    <AmplifyLink onClick={() => navigate('/Dashboard')}> Dashboard</AmplifyLink>
+                    <AmplifyLink onClick={() => navigate('/Submission')}>Request a video</AmplifyLink>
                 </Flex>
                 {route !== 'authenticated' ? (
                     <Button onClick={() => navigate('/Login')}> Login</Button>
-                ):
+                ) :
                     <Flex direction='row' alignItems='center'>
-                        <AmplifyLink onClick={()=> navigate('/Profile')}>Hello, {user.attributes.name}!</AmplifyLink>
+                        <AmplifyLink onClick={() => navigate('/Profile')}>Hello, {user.attributes.name}!</AmplifyLink>
                         <Button onClick={() => logOut()}> Sign Out</Button>
                     </Flex>
                 }
             </Flex>
-            <Outlet/>
+            <Outlet />
         </View>
     )
 }
