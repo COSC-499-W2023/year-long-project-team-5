7b4a9d94957370676ef3dc692734a9093f65b57b
Abhinav Thota <abhinavthota01@gmail.com>
2024-01-14 18:59:04 -0800

    reviewed code with Josh and added further UI changes: changed TextField styling, added labels, changed margins/paddings and changed heading.

--- app/amplifyapp/src/pages/Submission.js
+++ app/amplifyapp/src/pages/Submission.js
@@ -70,47 +70,38 @@ export function Submission(){
       const { tokens } = useTheme();
     return(
         <View className="App">
-        <Heading level={1}>Request a video</Heading>
-        <View as="form" margin="1rem 3rem" alignContent = "center" onSubmit={createNote} padding={tokens.space.medium}>
-        <Flex alignItems="center" justifyContent="center" height="50vh">
-          <Card variation="elevated" width="400px">
-          <Flex direction="column" justifyContent = "center" textAlign = "left">
-            <TextField
-              name="name"
-              placeholder="Recipient name"
-              label="name"
-              labelHidden
-              variation="quiet"
-              required
-            />
-            <TextField
-              name="name"
-              placeholder="Recipient email"
-              label="name"
-              labelHidden
-              variation="quiet"
-              required
-            />
-            <TextField
-              name="description"
-              placeholder="Instructions/notes"
-              label="Note Description"
-              labelHidden
-              variation="quiet"
-              required
-            />
-            {/*<TextField
-              name="company"
-              placeholder="Company name"
-              label="company"
-              labelHidden
-              variation="quiet"
-          />*/}
-           <Button type="submit" variation="primary">Request Video </Button>
-            </Flex>
+        <Heading level={2}>Video Request</Heading>
+        <View as="form" margin="2rem 3rem" alignContent = "center" onSubmit={createNote} padding={tokens.space.medium}>
+          <Flex alignItems="center" justifyContent="center" height="50vh">
+            <Card variation="elevated" width="30em" padding='1em'>
+              <Flex direction="column" justifyContent = "center" textAlign = "left" gap='2em' padding='1em'>
+                <TextField
+                  name="name"
+                  placeholder="Bilbo Baggins"
+                  label="Recipient Name"
+                  required
+                />
+                <TextField
+                  name="email"
+                  placeholder="bilbobaggins@mordor.com"
+                  label="Recipient Email"
+                  required
+                />
+                <TextField
+                  name="description"
+                  placeholder="Instructions/notes"
+                  label="Video Instructions"
+                  inputStyles={{
+                    paddingBottom: "5em",
+                  }}
+                  required
+                />
+              <Button type="submit" variation="primary">Request Video </Button>
+              </Flex>
             </Card>
-            </Flex>
+          </Flex>
         </View>
+          
        {/* Will enable popup once submission code is finalized
       <Popup open={isFormSubmitted} modal closeOnDocumentClick>
         <View>
