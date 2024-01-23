42f8b4cc18e21dfc3fae43f69ef22f076156ed5e
abbykiehlbauch <abbykiehlbauch@gmail.com>
2024-01-23 14:14:46 -0800

    make navbar into one component to accomodate light and dark mode

--- app/amplifyapp/src/my-components/NavBar.jsx
+++ app/amplifyapp/src/my-components/NavBar.jsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
 import DarkLightToggle from "../my-components/DarkLightToggle"
-import {Button, defaultDarkModeOverride, ToggleButton, Theme, ThemeProvider, useTheme, useAuthenticator,View, Flex, Link as AmplifyLink } from '@aws-amplify/ui-react';
+import {Button, defaultDarkModeOverride, ToggleButton, Theme, ThemeProvider, useTheme, useAuthenticator,View, Flex, Link as AmplifyLink, Menu, MenuItem } from '@aws-amplify/ui-react';
 /**
  * NavBar component used to display NavBar on larger displays (desktop), 
  * for users in both a logged in and logged out state
@@ -22,18 +22,57 @@ const NavBar = (props) => {
         signOut();
         navigate('/');
     }
+    const [isMobile, setIsMobile] = useState(window.innerWidth <= 1000);
+    useEffect(() => {
+        const handleResize = () => {
+          setIsMobile(window.innerWidth <= 1000);
+        };
+        window.addEventListener('resize', handleResize);
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
     const theme = {
         name: 'my-theme',
         overrides: [defaultDarkModeOverride],
       };
-      const [colorMode, setColorMode] = React.useState('light');
+    const [colorMode, setColorMode] = React.useState('light');
     const {tokens} = useTheme();
     React.useState('light');
     return (
-        <ThemeProvider theme = {theme} colorMode = {colorMode}>
         <View
-        class = "App" backgroundColor={tokens.colors.background.primary}
+         backgroundColor={tokens.colors.background.primary}
         >
+           {isMobile ? (
+            <Flex backgroundColor = {tokens.colors.primary} boxShadow={tokens.shadows.medium} padding={tokens.space.small} justifyContent='space-between' alignItems='center' marginBottom={tokens.space.large}>
+                 
+                 {route !== 'authenticated' ? (
+                 <Menu direction = 'column'>    
+                     <MenuItem onClick={()=> navigate('/')}> Home</MenuItem>
+                 </Menu>
+                 ):
+                 <Menu>       
+                     <MenuItem onClick={()=> navigate('/')}> Home</MenuItem>
+                     <MenuItem onClick={()=> navigate('/Dashboard')}>Dashboard</MenuItem>
+                     <MenuItem onClick={()=> navigate('/Submission')}>Request a video</MenuItem>
+                 </Menu>
+                 }
+                 
+                 {route !== 'authenticated' ? (
+                      <Flex direction='row' alignItems='center'>
+                      <DarkLightToggle colorMode={colorMode} setColorMode={setColorMode}/>
+                      <Button onClick={() => navigate('/Login')}> Login</Button>
+                  </Flex>
+                 ): 
+                 <Flex direction='row' alignItems='center'>
+                     <DarkLightToggle colorMode={colorMode} setColorMode={setColorMode}/>
+                     <AmplifyLink onClick={()=> navigate('/Profile')}>Hello, {user.attributes.name}!</AmplifyLink>
+                     <Button onClick={() => logOut()}> Sign Out</Button>
+                 </Flex>
+                 }
+                 </Flex>
+           ):
+           
            <Flex backgroundColor = {tokens.colors.primary} boxShadow={tokens.shadows.medium} padding={tokens.space.small} justifyContent='space-between' alignItems='center' marginBottom={tokens.space.large}>
                 <Flex direction="row">
                     {route !== 'authenticated' ? (
@@ -59,9 +98,9 @@ const NavBar = (props) => {
                 </Flex>
                 }
             </Flex>
+}
             <Outlet/>
         </View>
-        </ThemeProvider>
     )
 }
 
--- app/amplifyapp/src/pages/Layout.js
+++ app/amplifyapp/src/pages/Layout.js
@@ -1,9 +1,6 @@
-import { Outlet, useNavigate } from "react-router-dom";
 import NavBar from '../my-components/NavBar';
-import  NavBarMobile  from '../my-components/NavBarMobile';
-import DarkLightToggle from "../my-components/DarkLightToggle"
-import React, { useState, useEffect } from "react";
-import {Button, ToggleButton, Theme,useTheme, ThemeProvider, useAuthenticator,View, Flex, Tabs, TabItem, Text, defaultDarkModeOverride, Link as AmplifyLink} from '@aws-amplify/ui-react';
+import React from "react";
+import {useTheme, ThemeProvider,  View,  defaultDarkModeOverride, Link as AmplifyLink} from '@aws-amplify/ui-react';
 
 /**
  * Layout TODO: finish docs
@@ -13,42 +10,16 @@ import {Button, ToggleButton, Theme,useTheme, ThemeProvider, useAuthenticator,Vi
  * <Layout></Layout>
  */
 export function Layout(){
-    
     const theme = {
       name: 'my-theme',
       overrides: [defaultDarkModeOverride],
     };
-    const {user, route, signOut} = useAuthenticator((context) => [
-        context.user,
-        context.route,  
-        context.signOut,
-    ]);
-    const navigate = useNavigate();
-    function logOut() {
-        signOut();
-        navigate('/login');
-    }
-    const [isMobile, setIsMobile] = useState(window.innerWidth <= 1000);
     const {tokens} = useTheme();
     const [colorMode, setColorMode] = React.useState('light');
-
-    useEffect(() => {
-        const handleResize = () => {
-          setIsMobile(window.innerWidth <= 1000);
-        };
-        window.addEventListener('resize', handleResize);
-        return () => {
-          window.removeEventListener('resize', handleResize);
-        };
-      }, []);
     return(
         <ThemeProvider theme={theme} colorMode={colorMode}>
         <View className="App" backgroundColor={tokens.colors.background.primary}>
-            {isMobile ? (
-                    <NavBarMobile/>
-                ):
             <NavBar/>    
-            }
         </View>
         </ThemeProvider>
     )
