418aa02c405768c90f268e76406cf5c72f62d7d3
Joshua Medina-Quiaro <joshuamed27@gmail.com>
2024-01-23 13:16:53 -0800

    Added cypress testing for submission card component, removed extra sum jest test

--- app/amplifyapp/src/pages/Dashboard.js
+++ app/amplifyapp/src/pages/Dashboard.js
@@ -43,8 +43,6 @@ import awsconfig from '../aws-exports';
  * <Dashboard></Dashboard>
  */
 export function Dashboard() {
-  const { user, route } = useAuthenticator((context) => [context.user, context.route]);
-  console.log(Auth.user.username)
   const [notes, setNotes] = useState([]);
   const [filteredNotes, setFilteredNotes] = useState([])
   const [isMobile, setIsMobile] = useState(window.innerWidth <= 1000);
@@ -180,3 +178,4 @@ export function Dashboard() {
     </View>
   )
 }
+export default Dashboard;
