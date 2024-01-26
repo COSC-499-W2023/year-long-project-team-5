7425f97ac4c269b90144db730dd084cea21a26f2
abbykiehlbauch <abbykiehlbauch@gmail.com>
2024-01-25 16:17:15 -0800

    make profile info component and modify existing profile page

---
+++ app/amplifyapp/src/my-components/Profile.jsx
@@ -0,0 +1,83 @@
+import React, {  } from "react";
+import "../App.css";
+import "@aws-amplify/ui-react/styles.css";
+import { useNavigate } from "react-router-dom";
+import { useAuthenticator} from "@aws-amplify/ui-react"
+
+import {
+    Button,
+    Flex,
+    TextField,
+    View,
+    Heading,
+    Card, 
+    useTheme
+  } from '@aws-amplify/ui-react';
+
+export const ProfileInfo = (props) => {
+    const { tokens } = useTheme();
+
+    return (
+    <View backgroundColor={tokens.colors.background.primary}>
+        <Heading level={1}>Welcome, {props.name}!</Heading>
+        <Flex direction = 'column' width = '100%' justifyContent='space-evenly' alignItems='center' marginTop = '2rem' rowGap='2.5rem'>
+            <View as="form" minWidth ='80%'>
+                <Heading textAlign = 'left' marginBottom='1rem' level={4}>Edit Profile</Heading>
+                <Card variation="elevated">
+                <Flex direction="column" justifyContent = "center" textAlign = "left">
+                {<TextField
+                    name="name"
+                    placeholder={props.name}
+                    label="Name"
+                    variation="default"
+                    required
+                />}
+                    <TextField
+                    name="name"
+                    placeholder={props.email}
+                    label="Email"
+                    variation="default"
+                    required
+                    isDisabled
+                    />
+                    {<TextField
+                    name="company"
+                    placeholder="Company name"
+                    label="Company Name"
+                    variation="default"
+                    />}
+                <Button type="submit" variation="primary">Save Changes </Button>
+                    </Flex>
+                    </Card>
+                </View>
+                <View as="form" minWidth ='80%'>
+                <Heading textAlign = 'left' marginBottom='1rem' level={4}>Change password</Heading>
+                <Card variation="elevated">
+                <Flex direction="column" justifyContent = "center" textAlign = "left">
+                {<TextField
+                    name="currPassword"
+                    label="Enter your current password"
+                    variation="default"
+                    required
+                />}
+                    <TextField
+                    name="newPass1"
+                    label="Enter your new password"
+                    variation="default"
+                    required
+                    />
+                    {<TextField
+                    name="newPass2"
+                    label="Confirm your new password"
+                    variation="default"
+                    required
+                    />}
+                <Button type="submit" variation="primary">Change Password</Button>
+                    </Flex>
+                    </Card>
+                </View>
+            </Flex>
+        </View>
+    )
+}
+export default ProfileInfo;
