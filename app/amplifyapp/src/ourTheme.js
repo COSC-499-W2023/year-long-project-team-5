be232a8457bd190d4132521a5647e07e3f574244
Abhinav Thota <abhinavthota01@gmail.com>
2024-02-01 15:53:54 -0800

    update theming file; think it might be better to override amplify variables

--- app/amplifyapp/src/ourTheme.js
+++ app/amplifyapp/src/ourTheme.js
@@ -1,56 +1,98 @@
-import { defaultTheme } from '@aws-amplify/ui-react';
+// import { defaultTheme } from '@aws-amplify/ui-react';
 const ourTheme = {
-    name: 'my-theme',
+    name: 'our-theme',
+    tokens: {
+        borderWidths: {
+            small: { value: '2px' },
+            medium: { value: '4px' },
+            large: { value: '8px' },
+            },
+            radii: {
+            xs: { value: '1rem' },
+            small: { value: '2rem' },
+            medium: { value: '2rem' },
+            large: { value: '2rem' },
+            xl: { value: '3rem' },
+        },
+    },
     overrides: 
     [{
         colorMode: 'dark',
         tokens: {
             colors: {
+                primary:{
+                    10: { value: "#8a92be"},
+                    20: { value: "#5a64a2"},
+                    40: { value: "#515a92" },
+                    80: { value: "#485081" },
+                    90: { value: "#3f4671" },
+                    100: { value: "#363c61"},
+                },
+                secondary:{
+                    10: { value: "#e9b6c6" },
+                    20: { value: "#dc92aa" },
+                    40: { value: "#d57f9d" },
+                    80: { value: "#cd6d8f" },
+                    90: { value:  "#c55982"},
+                    100: { value: "#b22d69"},
+                },
+                font: {
+                    primary: {value: "#faedf0" },
+                    secondary:{value: "#424769"},
+                    tertiary: {value: "#7077A1"},
+                    quaternary: {},
+                    interactive: {value: "#e3a4b8"},
+                    disabled:{},
+                    hover: { value: "#f5dae2"},
+                    info:{value: "#7570a1"},
+                    warning: {value: "#fda194"},
+                    error: {value: "#a17077"},
+                    success: {value: "#77a170"}
+                },
                 background:{
-                    secondary:{value: "#0D1A26"}
+                    primary: {value: "#2d3250" },
+                    secondary:{value: "#424769"},
+                    tertiary: {value: "#7077A1"},
+                    quaternary: {},
+                    disabled:{},
+                    info:{value: "#7570a1"},
+                    warning: {value: "#fda194"},
+                    error: {value: "#a17077"},
+                    success: {value: "#77a170"}
+                },
+                border:{
+                    primary: {value: "#fff"},
+                    secondary: {value: "#dc92aa"},
+                    tertiary: {value: "#e3a4b8"},
+                    disabled: {value: "#8c757c"},
+                    pressed:{value: "#e3a4b8"},
+                    focus:{value: "#e3a4b8"},
+                    error:{value: "#e72f42"},
+                    info:{value: "#faedf0"},
+                    success:{value: "#00945a"},
+                    warning: {value: "#c8513a"}
                 },
                 neutral: {
-                  10: { value: defaultTheme.tokens.colors.neutral[100].value },
-                  20: { value: defaultTheme.tokens.colors.neutral[90].value },
-                  40: { value: defaultTheme.tokens.colors.neutral[80].value },
-                  80: { value: defaultTheme.tokens.colors.neutral[40].value },
-                  90: { value: defaultTheme.tokens.colors.neutral[20].value },
-                  100: { value: defaultTheme.tokens.colors.neutral[10].value },
+                  100: { value: "#ebebf0" },
+                  90: { value: "#d8d8e1" },
+                  80: { value: "#7c7d98"},
+                  40: { value: "#6a6c8a" },
+                  20: { value: "#585b7c" },
+                  10: { value: "#474b6e" },
                 },
-                white: { value: '#000' }, // set white to black,and black to white (inverse)
-                black: {value: '#fff'}
             },
-            components:{
+            components: {
                 button:{
                     primary:{
-                        backgroundColor: {value: "{colors.purple.60}"},
+                        backgroundColor: {value: "{colors.secondary.20}"},
+                        color: {value: "{colors.white}"},
                         hover: {
-                            backgroundColor: {value: "{colors.purple.80}"},
-                            color: {value: "{colors.black}"}
-                        },
-                        color: {value: "{colors.black}"}
-                    }
-                },
-                table: {
-                    row: {
-                        hover: {
-                            backgroundColor: { value: '{colors.overlay.40}' },
-                        },
-                        striped: {
-                            backgroundColor: { value: '{colors.neutral.10}' },
-                        },
+                            color:{value: "{colors.white}"} ,
+                            backgroundColor:{value: "{colors.secondary.10}"} 
+                        }
                     },
-                    header: {
-                        color: { value: '{colors.purple.60}' },
-                        fontSize: { value: '{fontSizes.medium}' },
-                        fontWeight: {value: '{fontWeights.medium}'},
-                    },
-                    data: {
-                        fontWeight: { value: '{fontWeights.light}' },
-                    },
-                },
+                }
             }
-            
         },
     },
     // light mode tokens:
