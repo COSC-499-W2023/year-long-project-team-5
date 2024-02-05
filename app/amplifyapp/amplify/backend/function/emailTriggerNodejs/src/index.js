b5745450c61f5eb3c3ea48ac360f62f7d373b8bc
MooMan <36575231+Namesnipes@users.noreply.github.com>
2024-02-04 20:03:37 -0800

    Now can send a test email to a hardcoded address from lambda

--- app/amplifyapp/amplify/backend/function/emailTriggerNodejs/src/index.js
+++ app/amplifyapp/amplify/backend/function/emailTriggerNodejs/src/index.js
@@ -12,11 +12,13 @@ import { defaultProvider } from '@aws-sdk/credential-provider-node';
 import { SignatureV4 } from '@aws-sdk/signature-v4';
 import { HttpRequest } from '@aws-sdk/protocol-http';
 import { default as fetch, Request } from 'node-fetch';
+import { SESClient, SendEmailCommand } from "@aws-sdk/client-ses";
 
 const GRAPHQL_ENDPOINT = process.env.API_YEARLONGPROJECTTEAM5_GRAPHQLAPIENDPOINTOUTPUT;
 const AWS_REGION = process.env.AWS_REGION || 'ca-central-1';
 const { Sha256 } = crypto;
 
+const ses = new SESClient({ region: AWS_REGION });
 const query = /* GraphQL */ `
 query MyQuery {
   getSubmission(id: "d85e0053-19e2-49ea-98bb-f3b16bbabab8") {
@@ -30,13 +32,45 @@ query MyQuery {
 
 `;
 
+
+let send_email = async(body) => {
+  const command = new SendEmailCommand({
+    Destination: {
+      ToAddresses: ["corklebeck@gmail.com"],
+    },
+    Message: {
+      Body: {
+        Text: { Data: "test body" },
+      },
+
+      Subject: { Data: "Test Email" },
+    },
+    Source: "corklebeck@gmail.com",
+  });
+
+  try {
+    let response = await ses.send(command);
+    console.log("EMAIL RESPONSE:",response)
+    // process data.
+    return response;
+  }
+  catch (error) {
+    console.log("EMAIL ERROR:", error)
+    // error handling.
+  }
+  finally {
+    console.log("Finally done sending email")
+    // finally.
+  }
+};
+
+
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
 
 export const handler = async (event) => {
   console.log(`EVENT: ${JSON.stringify(event)}`);
-  console.log("URL THE URL IS", GRAPHQL_ENDPOINT)
   const endpoint = new URL(GRAPHQL_ENDPOINT);
 
   const signer = new SignatureV4({
@@ -65,6 +99,7 @@ export const handler = async (event) => {
   let response;
 
   try {
+    await send_email(body)
     response = await fetch(request);
     body = await response.json();
     if (body.errors) statusCode = 400;
