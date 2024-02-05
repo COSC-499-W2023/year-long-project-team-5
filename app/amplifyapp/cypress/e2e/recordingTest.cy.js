87756ecaf979fab7b0c93b889cfad2eb6b1a57ef
Abhinav Thota <abhinavthota01@gmail.com>
2024-02-05 15:34:50 -0800

    added test to time the delay for record button to be found.

--- app/amplifyapp/cypress/e2e/recordingTest.cy.js
+++ app/amplifyapp/cypress/e2e/recordingTest.cy.js
@@ -7,4 +7,18 @@ describe('test recording button', () => {
             cy.get('.recordButton').click()
           )
     })
+    it('visits recording page and sees if there is a delay for button to be rendered - should be ~500ms', () => {
+        let startTime, endTime = 0
+        cy.visit('localhost:3000/Recording')
+        cy.window().then(win => {
+            startTime = win.performance.now();
+            cy.get('.recordButton').should('exist')
+              .then(() => {
+                  endTime = win.performance.now()
+              })
+              .then(() => {
+                const duration = endTime- startTime;
+                console.log(`Operation took ${duration} milliseconds.`)});
+        });
+    })
 })
