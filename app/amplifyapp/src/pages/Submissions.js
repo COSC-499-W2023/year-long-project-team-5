fe97f9633e60242add0ca66a3e0a594d597db803
Joshua Medina-Quiaro <joshuamed27@gmail.com>
2024-03-30 14:53:39 -0700

    Add case for video loading URL

--- app/amplifyapp/src/pages/Submissions.js
+++ app/amplifyapp/src/pages/Submissions.js
@@ -87,9 +87,13 @@ export function Submissions() {
     await Promise.all(
       filteredSubmissions.map(async (submission) => {
         if (submission.Video && submission.Video.videoURL) {
-          const url = await Storage.get(submission.Video.videoURL.replace("toBlur/", ""));
-          submission.Video.videoName = submission.Video.videoURL;
-          submission.Video.videoURL = url;
+          if (await Storage.getProperties(submission.Video.videoURL)) {
+            const url = await Storage.get(submission.Video.videoURL.replace("toBlur/", ""));
+            submission.Video.videoName = submission.Video.videoURL;
+            submission.Video.videoURL = url;
+          } else {
+            submission.Video.videoURL = 'loadingBlur';
+          }
         }
         return submission;
       })
